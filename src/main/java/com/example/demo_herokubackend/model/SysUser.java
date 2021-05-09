3af28028986b701e972752b4ab66df6487f4849b
phi <daovanphi25051994@gmail.com>
2021-05-10 00:55:02 +0700

    them postgresql

--- src/main/java/com/example/demo_herokubackend/model/SysUser.java
+++ src/main/java/com/example/demo_herokubackend/model/SysUser.java
@@ -2,10 +2,7 @@ package com.example.demo_herokubackend.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -13,6 +10,8 @@ public class SysUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
     private String name;
 
 }
